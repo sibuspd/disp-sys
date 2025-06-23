668e15ba5e3be7ddb6ba97088eee54d95016b60b
Sabyasachi Sahani <sibuspd@gmail.com>
2025-06-23 18:25:44 +0530

    Delete Modal added

--- frontendDispensary/src/pages/Admin/Gallery/DeleteModal/DeleteModal.jsx
+++ frontendDispensary/src/pages/Admin/Gallery/DeleteModal/DeleteModal.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const DeleteModal = (props) => {
   return (
-        <div className='addModal'>
+    <div className="addModal">
       <div className="addModal-card">
         <div>Delete Image</div>
         <div className="modal-add-btns">
-          <div className='cancel-modal-btn' onClick={() => props.onClose()}>Cancel</div>
-          <div className='cancel-modal-btn'><DeleteIcon/></div>
+          <div className="cancel-modal-btn" onClick={() => props.onClose()}>
+            Cancel
+          </div>
+          <div className="cancel-modal-btn">
+            <DeleteIcon />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DeleteModal
+export default DeleteModal;
