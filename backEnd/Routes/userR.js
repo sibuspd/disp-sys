e0c5e4278db779db53d63b3ced6ab3c16adf9a5b
sibuspd <sibuspd@gmail.com>
2025-07-25 22:52:54 +0530

    Added route for Staff-initiated Student's regd

--- backEnd/Routes/userR.js
+++ backEnd/Routes/userR.js
@@ -9,8 +9,12 @@ router.post('/login', UserController.login );
 router.post('/send-otp', UserController.sendOtp);
 router.post('/verify-otp', UserController.verifyOtp);
 router.post('/reset-password', UserController.resetPassword);
+router.post('/registerStudentByStaff', Authentication.adminFacultyAuth, UserController.registerStudentByStaff); // Only admin or faculty can register students
 
 //PUT API Calls
 router.put('/update-student/:id', Authentication.adminFacultyAuth, UserController.updateStudentById); // middleware has been passed to check if the user is an admin or faculty before allowing update
+
+// GET API Calls
+router.get('/get-student-by-roll/:roll', Authentication.adminFacultyAuth, UserController.getStudentByRollNo); // A student should not be able to access some other roll no other than his own
 
 module.exports = router;
