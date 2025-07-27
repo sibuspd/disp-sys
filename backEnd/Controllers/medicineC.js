630d759af7c35f69a10bc539f94a90c2c15af459
sibuspd <sibuspd@gmail.com>
2025-07-27 21:02:26 +0530

    Update Medicine feature added

--- backEnd/Controllers/medicineC.js
+++ backEnd/Controllers/medicineC.js
@@ -8,7 +8,45 @@ exports.addMedicine = async (req, res) => {
        await medicine.save();
        
        res.status(201).json({ message: 'Medicine added successfully', medicine });
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
 
+// Function to get all medicines
+exports.getMedicine = async (req, res) => {
+    try{
+        const medicines = await MedicineModels.find().populate('addedBy','name').sort({createdAt: -1});
+        return res.status(200).json({
+            message: 'Medicines retrieved successfully',
+            medicines: medicines
+        });
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ 
+            err: 'Internal server error',
+            issue: err.message
+         });
+    }
+}
+
+// Function to update medicine
+exports.updateMedicineById = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const body = {...req.body};
+
+        const medicine = await MedicineModels.findByIdAndUpdate(id, {...body, addedBy: req.user._id}, {new:true});
+        if(medicine){
+            return res.status(200).json({
+                message: 'Medicine updated successfully',
+                medicine: medicine
+            });
+        }
+        return res.status(404).json({ error: 'No such Medicine exists in the database'});
     }
     catch(err){
         console.error(err);
--- backEnd/Routes/medicineR.js
+++ backEnd/Routes/medicineR.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const Authentication = require('../Authentication/auth');
 const MedicineController = require('../Controllers/medicineC');
 
+// POST API Calls
 router.post('/add', Authentication.adminFacultyAuth, MedicineController.addMedicine);
+
+//GET API Calls
+router.get('/get', Authentication.adminFacultyAuth, MedicineController.getMedicine);
+
+// PUT API Calls
+router.put('/update/:id', Authentication.adminFacultyAuth, MedicineController.updateMedicineById);
 
 module.exports = router;
