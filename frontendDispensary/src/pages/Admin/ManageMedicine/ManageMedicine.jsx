d094891ced9f136b224c3ebbbfcf08acdadfe52c
sibuspd <sibuspd@gmail.com>
2025-08-18 01:38:34 +0530

    Display medicine feature integrated

--- frontendDispensary/src/pages/Admin/ManageMedicine/ManageMedicine.jsx
+++ frontendDispensary/src/pages/Admin/ManageMedicine/ManageMedicine.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./manageMedicine.css";
 import { Link } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SearchBox from "../../../components/SearchBox/SearchBox";
 import EditIcon from "@mui/icons-material/Edit";
-import Modal from "../../../components/Modal/Modal"; 
+import Modal from "../../../components/Modal/Modal";
 import MedicineModal from "./MedicineModal/MedicineModal";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 
-function ManageMedicine() {
+function ManageMedicine(props) {
   const [medicineSearch, setMedicineSearch] = useState("");
-  const [addModal, setAddModal] =   useState(false);
+  const [addModal, setAddModal] = useState(false);
+
+  const [data, setData] = useState([]); // For fetching medicines from backend
 
   const onOffModal = () => {
-    setAddModal( prev => !prev);
-  }
+    setAddModal((prev) => !prev);
+  };
 
   const onChangeValue = (value) => {
     setMedicineSearch(value);
   };
+
+  const fetchData = async () => {
+    props.showLoader();
+    await axios
+      .get(
+        `http://localhost:4000/api/medicine/search-by-name?name=${medicineSearch}`
+      )
+      .then((response) => {
+        setData(response.data.medicines);
+      })
+      .catch((err) => {
+        toast.error(err?.response?.data?.error);
+      })
+      .finally(() => {
+        props.hideLoader();
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [medicineSearch]);
+
   return (
     <div className="manageMedicine">
       <div className="go-back">
@@ -33,7 +59,9 @@ function ManageMedicine() {
           value={medicineSearch}
           onChange={onChangeValue}
         />
-        <div className="add-manage-medicine" onClick={onOffModal}>Add</div>
+        <div className="add-manage-medicine" onClick={onOffModal}>
+          Add
+        </div>
       </div>
 
       <div className="manageMedicine-card">
@@ -49,27 +77,38 @@ function ManageMedicine() {
           </div>
           {/* Report Table Rows */}
           <div className="report-form-row-block">
-            <div className="report-form-row">
-              <div className="">2</div>
-              <div className="col-2-mng">Ibuprofen</div>
-              <div className="col-2-mng">Priyanka</div>
-              <div className="col-3-mng">34</div>
-              <div className="edit-icon">
-                <EditIcon />
+            {data.map((item, index) => {
+              return (
+                <div key={index} className="report-form-row">
+                  <div className="">{index + 1}</div>
+                  <div className="col-2-mng">{item.name}</div>
+                  <div className="col-2-mng">{item?.addedBy?.name}</div>
+                  <div className="col-3-mng">{item.quantity}</div>
+                  <div className="edit-icon">
+                    <EditIcon />
+                  </div>
+                  <div className="delete-icon">
+                    <DeleteIcon />
+                  </div>
+                </div>
+              );
+            })}
+            {data.length === 0 && (
+              <div className="report-form-row">
+                <div className="">No Medicine found.</div>
               </div>
-              <div className="delete-icon">
-                <DeleteIcon />
-              </div>
-            </div>
-            {/* Blank row */}
-            <div className="report-form-row">
-              <div className="">No Medicine has been added yet.</div>
-            </div>
+            )}
           </div>
         </div>
       </div>
-      { addModal && <Modal header="Manage Medicine" handleClose={onOffModal} children={<MedicineModal/>}/>}
-      
+      {addModal && (
+        <Modal
+          header="Manage Medicine"
+          handleClose={onOffModal}
+          children={<MedicineModal />}
+        />
+      )}
+      <ToastContainer />
     </div>
   );
 }
