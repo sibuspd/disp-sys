b711b11be87fc839fdc83560303f8ba2de53860d
sibuspd <sibuspd@gmail.com>
2025-08-19 00:29:22 +0530

    Hospitals API integrated

--- frontendDispensary/src/pages/Admin/NearByHospital/NearByHospital.jsx
+++ frontendDispensary/src/pages/Admin/NearByHospital/NearByHospital.jsx
@@ -1,21 +1,71 @@
-import React, { useState } from 'react'
-import "./nearByHospital.css"
+import React, { useState, useEffect } from "react";
+import "./nearByHospital.css";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Link } from "react-router-dom";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Modal from "../../../components/Modal/Modal";
-import NearByModal from './NearByModal/NearByModal';
+import NearByModal from "./NearByModal/NearByModal";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 
-const NearByHospital = () => {
+const NearByHospital = (props) => {
+  const [modal, setModal] = useState(false);
+  const [data, setData] = useState([]); // Fetching Hospital Data from backend
+  const [clickedItem, setClickedItem] = useState(null); // For Edit/Delete functionality
 
-    const [modal, setModal] = useState(false);
-
-    const onOffModal = () => {
-        setModal(prev => !prev);
+  const onOffModal = () => {
+    if(modal){
+      setClickedItem(null); // Reset clicked item when modal is closed
     }
+    setModal((prev) => !prev);
+  };
+
+  const fetchData = async () => {
+    props.showLoader();
+    await axios
+      .get("http://localhost:4000/api/hospital/get")
+      .then((response) => {
+        setData(response.data.hospitals);
+      })
+      .catch((err) => {
+        toast.error(err?.response?.data?.error);
+      })
+      .finally(() => {
+        props.hideLoader();
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleEdit = (item) => {
+    setClickedItem(item);
+    setModal(true);
+  }
+
+  const filterOutData = (id) => {
+    let newArr = data.filter((item)=> item._id !== id);
+    setData(newArr);
+  }
+  const handleDelete = async (id) => {
+    props.showLoader();
+    await axios.delete(`http://localhost:4000/api/hospital/delete/${id}`, { withCredentials: true })
+    .then((response)=>{
+      filterOutData(id);
+      toast.success(response.data.message);
+    })
+    .catch(err=>{
+      toast.error(err?.response?.data?.error);
+    })
+    .finally(()=>{
+      props.hideLoader();
+    });
+  }
+
   return (
-        <div className="admin-facility">
+    <div className="admin-facility">
       <div className="go-back">
         <Link to="/admin/dashboard">
           <ArrowBackIcon /> Back to Dashboard
@@ -24,51 +74,44 @@ const NearByHospital = () => {
 
       <div className="admin-facility-header">
         <div>Nearby Hospitals</div>
-        <div className="add-facility-btn" onClick={onOffModal}>Add</div>
+        <div className="add-facility-btn" onClick={onOffModal}>
+          Add
+        </div>
       </div>
 
       <div className="admin-facility-rows">
-        <div className="admin-facility-row">
-          <div className="admin-facility-left">
-            <div className="admin-facility-title">Name</div>
-            <div>
-              Address Burla NAC 
-            </div>
-            <div style={{ marginTop: "10px" }}>+91-xxx-xxx-xxxx</div>
-          </div>
+        {data.map((item, index) => {
+          return (
+            <div  key={index} className="admin-facility-row">
+              <div className="admin-facility-left">
+                <div className="admin-facility-title">{item.name}</div>
+                <div>{item.address}</div>
+                <div style={{ marginTop: "10px" }}>{item.contact}</div>
+                <div style={{ marginTop: "10px" }}>Added By: <b>{item?.addedBy?.name}</b></div>
+              </div>
 
-          <div className="admin-facility-btns">
-            <div>
-              <EditIcon />
+              <div className="admin-facility-btns">
+                <div onClick={()=>handleEdit(item)}>
+                  <EditIcon />
+                </div>
+                <div onClick={()=>handleDelete(item._id)}>
+                  <DeleteIcon />
+                </div>
+              </div>
             </div>
-            <div>
-              <DeleteIcon />
-            </div>
-          </div>
-        </div>
-                <div className="admin-facility-row">
-          <div className="admin-facility-left">
-            <div className="admin-facility-title">Name</div>
-            <div>
-              Address Burla NAC 
-            </div>
-            <div style={{ marginTop: "10px" }}>+91-xxx-xxx-xxxx</div>
-          </div>
-
-          <div className="admin-facility-btns">
-            <div>
-              <EditIcon />
-            </div>
-            <div>
-              <DeleteIcon />
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-      {modal && <Modal headers= "Add Facility" handleClose={onOffModal} children={<NearByModal/>}/> }
-      
+      {modal && (
+        <Modal
+          headers="Add Facility"
+          handleClose={onOffModal}
+          children={<NearByModal clickedItem={clickedItem}/>}
+        />
+      )}
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default NearByHospital  
+export default NearByHospital;
--- frontendDispensary/src/pages/Admin/NearByHospital/NearByModal/NearByModal.jsx
+++ frontendDispensary/src/pages/Admin/NearByHospital/NearByModal/NearByModal.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "./nearByModal.css"
+import axios from "axios";
+import {toast, ToastContainer} from "react-toastify";
 
-const NearByModal = () => {
+const NearByModal = (props) => {
 
   const [inputField, setInputField] = useState({name:"", address:"", contact:""});
 
@@ -9,9 +11,44 @@ const NearByModal = () => {
     setInputField({...inputField, [key]: event.target.value});
   }
 
+  const updateFunc = async () => {
+    await axios.put(`http://localhost:4000/api/hospital/update/${props.clickedItem._id}`, inputField, {withCredentials: true})
+    .then((response)=> {
+      window.location.reload();
+    })
+    .catch(err => {
+      toast.error(err?.response?.data?.error);
+    });
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if(inputField.name.trim().length === 0 || inputField.address.trim().length === 0 || inputField.contact.trim().length === 0) 
+      return toast.error("Please enter all the fields");
+
+    // Only update if clickedItem is present/ if Edit icon is clicked 
+    if(props.clickedItem){
+      updateFunc();
+      return;
+    }
+
+    // Add new hospital
+    await axios.post('http://localhost:4000/api/hospital/add', inputField, {withCredentials: true})
+    .then((response) => {
+      window.location.reload();
+    })
+    .catch(err=>{
+      toast.error(err?.response?.data?.error);
+    });
   }
+
+  useEffect(() => {
+    if(props.clickedItem){
+      setInputField({...inputField, name: props.clickedItem.name, address: props.clickedItem.address, contact: props.clickedItem.contact});
+    }
+  }, []);
+
   return (
             <form action="" className="register-form" onSubmit={handleSubmit}>
           <div className="register-form-div">
@@ -28,7 +65,7 @@ const NearByModal = () => {
               value={inputField.contact} onChange={(event) => handleOnChange(event, "contact")}/>
             </div>            
           </div>
-          <button type="submit" className="form-btn reg-btn">Add</button>
+          <button type="submit" className="form-btn reg-btn">{props.clickedItem?"Update":"Add"}</button>
         </form>
   )
 }
