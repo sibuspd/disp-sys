be7329e3c523c360a508946009bb64952898943f
sibuspd <sibuspd@gmail.com>
2025-08-23 16:40:30 +0530

    Student Panel API Integrated

--- frontendDispensary/src/pages/Student/StudentDashboard.jsx
+++ frontendDispensary/src/pages/Student/StudentDashboard.jsx
@@ -1,23 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./studentDashboard.css";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import Modal from "../../components/Modal/Modal";
 import StudentModal from "./StudentModal/StudentModal";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 
-const StudentDashboard = () => {
+const StudentDashboard = (props) => {
+  // Fetching userInfo from Local Storage
+  let userInfo = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
   const [modal, setModal] = useState(false);
+  const [history, setHistory] = useState([]); // State to hold medicine usage history of the student
+  const [selectedHistory, setSelectedHistory] = useState(null); // State to hold medicine history on a particular date
 
-  const handleOnOffModal = () => {
+  const handleOnOffModal = (item) => {
     setModal((prev) => !prev);
+    setSelectedHistory(item? item: null);
   };
+
+  const fetchData = async () => {
+    props.showLoader();
+    await axios
+      .get(`http://localhost:4000/api/history/get?roll=${userInfo?.roll}`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setHistory(response.data.history);
+      })
+      .catch((err) => {
+        toast.error(err?.response?.data?.error);
+      })
+      .finally(() => {
+        props.hideLoader();
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="student-dashboard">
       <div className="student-info">
         <div className="welcome-user">
-          Welcome, <span>Student</span>
+          Welcome, <span>{userInfo?.name}</span>
         </div>
-        <div className="welcome-user">3234</div>
-        <div className="welcome-user">xyz@yahoo.in</div>
+        <div className="welcome-user">{userInfo?.roll}</div>
+        <div className="welcome-user">{userInfo?.email}</div>
       </div>
 
       <div className="student-data">
@@ -27,27 +60,26 @@ const StudentDashboard = () => {
         </div>
 
         <div className="student-row-items">
-          <div className="student-row-item">
-            <div onClick={() => handleOnOffModal()}>
-              <RemoveRedEyeIcon sx={{ cursor: "pointer" }} />
-            </div>
-            <div>23-06-2025</div>
-          </div>
-          <div className="student-row-item">
-            <div onClick={() => handleOnOffModal()}>
-              <RemoveRedEyeIcon sx={{ cursor: "pointer" }} />
-            </div>
-            <div>23-06-2025</div>
-          </div>
-          <div className="student-row-item">
-            <div onClick={() => handleOnOffModal()}>
-              <RemoveRedEyeIcon sx={{ cursor: "pointer" }} />
-            </div>
-            <div>23-06-2025</div>
-          </div>
+          {history.map((item, index) => {
+            return (
+              <div key={index} className="student-row-item">
+                <div onClick={() => handleOnOffModal(item)}>
+                  <RemoveRedEyeIcon sx={{ cursor: "pointer" }} />
+                </div>
+                <div>{item.createdAt.slice(0,10).split("-").reverse().join("-")}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      {modal && <Modal header={"Medicine details"} handleClose={handleOnOffModal} children={<StudentModal/>}/>}
+      {modal && (
+        <Modal
+          header={"Medicine details"}
+          handleClose={handleOnOffModal}
+          children={<StudentModal  selectedHistory={selectedHistory} />}
+        />
+      )}
+      <ToastContainer />
     </div>
   );
 };
--- frontendDispensary/src/pages/Student/StudentModal/StudentModal.jsx
+++ frontendDispensary/src/pages/Student/StudentModal/StudentModal.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const StudentModal = () => {
+const StudentModal = (props) => {
   return (
-        <div className="record-modal">
-
+    <div className="record-modal">
       <div className="student-details-scroll">
         <div className="student-modal-detail">
           <div className="student-modal-header">
-            {new Date().toDateString()}
+            {props.selectedHistory?.createdAt
+              .slice(0, 10)
+              .split("-")
+              .reverse()
+              .join("-")}
           </div>
 
           <div className="student-modal-body-student">
@@ -17,20 +20,20 @@ const StudentModal = () => {
             </div>
 
             <div className="student-modal-body-item">
-              <div className="student-item-modal">
-                <div>Acetaminophen</div>
-                <div>23</div>
-              </div>
-              <div className="student-item-modal">
-                <div>Cefixime & Potassium Clauvunate</div>
-                <div>12</div>
-              </div>
+              {props.selectedHistory?.medicines.map((item, index) => {
+                return (
+                  <div key={index} className="student-item-modal">
+                    <div>{item?.name}</div>
+                    <div>{item?.requiredQuantity}</div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StudentModal
+export default StudentModal;
