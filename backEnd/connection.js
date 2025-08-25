dbf7d23048afec12bd64f50ac1eb8d30c3cf19fa
sibuspd <sibuspd@gmail.com>
2025-08-25 14:34:52 +0530

    connection string error fixed

--- backEnd/connection.js
+++ backEnd/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose') // Importing mongoose for MongoDB connection
 const dotenv = require('dotenv') // Importing dotenv for loading environment variables
 
-const mongoDBURL = "mongodb+srv://process.env.DB_USERNAME:process.env.DB_PASSWORD@cluster0.wkdzkh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" ; // Add your database URL
+const mongoDBURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wkdzkh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0` ; // Add your database URL
 
 // Connecting to MongoDB using mongoose
 mongoose.connect(mongoDBURL)
